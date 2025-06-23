f1b198c7b5bb1d0c9ef78f1e100bf0244a5a5dc6
YSMlearnsCode <yasin.m.syed@fau.de>
2025-06-23 21:58:43 +0200

    made ui hot reload

--- app.py
+++ app.py
@@ -1,17 +1,9 @@
-import gradio as gr
+from src.app.interface import main_interface
+
+demo = main_interface()
 
 def main():
-    with gr.Blocks() as demo:
-        gr.Markdown("# 🗑️ Mülltrenner9000 🤖")
-        gr.Markdown("🗑️ This app classifies and segments trash for proper recycling. 🤖")
-
-
-        with gr.Row():
-            input_image = gr.Image(type="numpy", label="Upload Image")
-            output_image = gr.Image(type="numpy", label="Segmented Output")
-        # input_image.change(fn=predict, inputs=input_image, outputs=output_image)
     demo.launch()
-
 
 if __name__ == "__main__":
     main()
---
+++ src/app/interface.py
@@ -0,0 +1,14 @@
+import gradio as gr
+
+
+def main_interface():
+    with gr.Blocks(title = "🗑️ Mülltrenner9000 🤖") as demo:
+        gr.Markdown("# 🗑️ Mülltrenner9000 🤖")
+        gr.Markdown("🗑️ This app classifies and segments trash for proper recycling according to german mülltrennung rules. 🤖")
+
+
+        with gr.Row():
+            input_image = gr.Image(type="numpy", label="Upload Image")
+            output_image = gr.Image(type="numpy", label="Segmented Output")
+        # input_image.change(fn=predict, inputs=input_image, outputs=output_image)
+    return demo
